5d786ba7c295edcc8663d5f82f0c4c16423641f4
Justin Golden <justingolden21@gmail.com>
2022-06-26 10:53:45 -0700

    AppCard implementation and cleanup

--- src/components/AppCard.svelte
+++ src/components/AppCard.svelte
@@ -1,17 +1,21 @@
 <script>
+	export let title, text, img, link, isNew, tags;
 </script>
 
-<div class="card w-96 bg-base-100 shadow-xl">
+<a href={link} target="_blank" class="card w-96 bg-base-200 shadow-xl cursor-pointer no-underline">
 	<div class="card-body">
 		<h2 class="card-title">
-			Shoes!
-			<div class="badge badge-secondary">NEW</div>
+			{title}
+			{#if isNew}
+				<div class="badge badge-secondary">NEW</div>
+			{/if}
 		</h2>
-		<p>If a dog chews shoes whose shoes does he choose?</p>
+		<p>{text}</p>
 		<div class="card-actions justify-end">
-			<div class="badge badge-outline">Fashion</div>
-			<div class="badge badge-outline">Products</div>
+			{#each tags as tag}
+				<div class="badge badge-outline">{tag}</div>
+			{/each}
 		</div>
 	</div>
-	<figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
-</div>
+	<figure><img src={img} alt={title} /></figure>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,6 +19,13 @@
 	<article class="prose lg:prose-xl">
 		<h1 class="text-center">RGB Studios</h1>
 		<p>Hello there</p>
-		<AppCard />
+		<AppCard
+			title="Desktop Clock"
+			text="A simple, resizable customizable, installable clock for any device!"
+			img="https://desktopclock.app/img/screenshot.png"
+			link="https://desktopclock.app/"
+			isNew={true}
+			tags={['clock', 'stopwatch', 'worldclock', 'web app', 'installable', 'free', 'offline']}
+		/>
 	</article>
 </div>
