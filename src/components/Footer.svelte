266e66c785581f73d6fc1f1c82ac58cec0bc2457
Justin Golden <justingolden21@gmail.com>
2022-06-26 14:38:52 -0700

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Icon from './Icon.svelte';
+	const year = new Date().getFullYear();
 </script>
 
 <footer class="w-full">
@@ -12,14 +13,14 @@
 			<a class="link link-hover" href="/projects?sort=updated">Recently Updated</a>
 		</div>
 		<div>
-			<span class="footer-title">Blog</span>
+			<!-- <span class="footer-title">Blog</span>
 			<a class="link link-hover">Web Development</a>
-			<a class="link link-hover">Design</a>
+			<a class="link link-hover">Design</a> -->
 		</div>
 		<div>
-			<span class="footer-title">Services</span>
+			<!-- <span class="footer-title">Services</span>
 			<a class="link link-hover">Branding</a>
-			<a class="link link-hover">Design</a>
+			<a class="link link-hover">Design</a> -->
 
 			<span class="footer-title mt-4">Company</span>
 			<a class="link link-hover" href="/about">About us</a>
@@ -30,12 +31,13 @@
 		<div class="items-center grid-flow-col">
 			<img class="w-12 h-12" src="/img/rgb_logo_mono_64.png" alt="RGB logo" />
 			<p>
-				<a class="link link-hover font-bold" href="/">RGB Studios</a><br />Simple webapps and more
+				<a class="link link-hover font-bold" href="/">RGB Studios</a><br />Simple webapps and more<br
+				/>&copy; Copyright {year}
 			</p>
 		</div>
 		<div>
-			<a class="link link-hover">Sitemap</a>
-			<a class="link link-hover">Privacy policy</a>
+			<a class="link link-hover" href="/sitemap">Sitemap</a>
+			<a class="link link-hover" href="/privacy">Privacy policy</a>
 		</div>
 		<div class="md:place-self-center md:justify-self-end">
 			<div class="grid grid-flow-col gap-4">
