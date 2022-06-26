d182c6d6865dbabda7d529fed70967d652076f67
Justin Golden <justingolden21@gmail.com>
2022-06-26 14:57:31 -0700

    Implement featured app carousel

---
+++ src/components/FeaturedAppCarousel.svelte
@@ -0,0 +1,23 @@
+<script>
+	import FeaturedAppCard from './FeaturedAppCard.svelte';
+	export let slides;
+</script>
+
+<div class="carousel w-full">
+	{#each slides as { id, app }, idx}
+		<div {id} class="carousel-item relative w-full">
+			<FeaturedAppCard {...app} />
+
+			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+				<a
+					href="#{slides[idx === 0 ? slides.length - 1 : idx - 1].id}"
+					class="btn btn-circle bg-white border-0">❮</a
+				>
+				<a
+					href="#{slides[idx === slides.length - 1 ? 0 : idx + 1].id}"
+					class="btn btn-circle bg-white border-0">❯</a
+				>
+			</div>
+		</div>
+	{/each}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
-	import FeaturedAppCard from '../components/FeaturedAppCard.svelte';
+	// import FeaturedAppCard from '../components/FeaturedAppCard.svelte';
+	import FeaturedAppCarousel from '../components/FeaturedAppCarousel.svelte';
 
 	import projects from '../data/projects';
 
@@ -10,16 +11,51 @@
 	<title>RGB Studios.org</title>
 </svelte:head>
 
-<FeaturedAppCard
+<!-- <FeaturedAppCard
 	title={featured.title}
 	text={featured.text}
 	longText={featured.longText}
 	img={featured.img}
 	link={featured.link}
+/> -->
+
+<FeaturedAppCarousel
+	slides={[
+		{
+			id: 'clock',
+			app: {
+				title: featured.title,
+				text: featured.text,
+				longText: featured.longText,
+				img: featured.img,
+				link: featured.link
+			}
+		},
+		{
+			id: 'notes',
+			app: {
+				title: 'notes',
+				text: 'text',
+				longText: 'longText',
+				img: 'http://placekitten.com/200/300',
+				link: 'google.com'
+			}
+		},
+		{
+			id: 'notes2',
+			app: {
+				title: 'notes2',
+				text: 'text2',
+				longText: 'longText2',
+				img: 'http://placekitten.com/200/200',
+				link: 'google.com/'
+			}
+		}
+	]}
 />
 
 <article class="prose lg:prose-xl mx-auto mt-8">
-	<h1>Who We Are</h1>
+	<h1>What We Do</h1>
 	<p>
 		RGB Studios makes awesome websites, webapps, mobile apps, and browser extensions. We take pride
 		in clean, beautiful designs that are simple and easy to use.
