a9361e571e01bd787d35bf197cce4d92c93a51ae
Justin Golden <justingolden21@gmail.com>
2022-06-26 12:27:07 -0700

    Create __error.svelte

---
+++ src/routes/__error.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export const load = ({ error, status }) => ({
+		props: {
+			error,
+			status
+		}
+	});
+</script>
+
+<script>
+	export let error, status;
+</script>
+
+<svelte:head>
+	<title>Error {status === 404 ? ' 404' : ''}</title>
+</svelte:head>
+
+<div class="container mx-auto py-8">
+	<article class="prose lg:prose-xl mx-auto">
+		{#if status === 404}
+			<h1 class="text-center">The resource could not be found</h1>
+		{/if}
+
+		<p>{error.name} {status} &mdash; {error.message}</p>
+
+		<a href="/" class="btn btn-primary no-underline">Go back home</a>
+	</article>
+</div>
