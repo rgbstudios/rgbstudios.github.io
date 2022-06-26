af88e25c1b3dea2fa2516e2de1396a08584f4625
Justin Golden <justingolden21@gmail.com>
2022-06-26 14:38:50 -0700

    Create privacy.svelte

---
+++ src/routes/privacy.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Privacy Policy | RGB Studios.org</title>
+</svelte:head>
+
+<article class="prose lg:prose-xl mx-auto">
+	<h1>Privacy Policy</h1>
+	<p>
+		All of our applications are currently free and without ads. We will never sell your data to
+		third parties or store sensitive data. We use local storage to save user preferences for
+		settings in some of our apps. We use Google Analytics to get usage data and improve our
+		products.
+	</p>
+	<p>
+		For any questions, you can contact us at <a href="mailto:contact@rgbstudios.org"
+			>contact@rgbstudios.org</a
+		>
+	</p>
+</article>
