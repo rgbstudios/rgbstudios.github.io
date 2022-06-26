fb665ae65523e4df76b15d5119fd627c1da89677
Justin Golden <justingolden21@gmail.com>
2022-06-26 15:02:58 -0700

    Support for only one featured app in carousel

--- src/components/FeaturedAppCarousel.svelte
+++ src/components/FeaturedAppCarousel.svelte
@@ -3,21 +3,27 @@
 	export let slides;
 </script>
 
-<div class="carousel w-full">
-	{#each slides as slide, idx}
-		<div id={idx} class="carousel-item relative w-full">
-			<FeaturedAppCard {...slide} />
+{#if slides.length === 1}
+	<FeaturedAppCard {...slides[0]} />
+{:else if slides.length > 1}
+	<div class="carousel w-full">
+		{#each slides as slide, idx}
+			<div id={idx} class="carousel-item relative w-full">
+				<FeaturedAppCard {...slide} />
 
-			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-				<a
-					href="#{idx === 0 ? slides.length - 1 : idx - 1}"
-					class="btn btn-circle bg-white border-0">❮</a
+				<div
+					class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
 				>
-				<a
-					href="#{idx === slides.length - 1 ? 0 : idx + 1}"
-					class="btn btn-circle bg-white border-0">❯</a
-				>
+					<a
+						href="#{idx === 0 ? slides.length - 1 : idx - 1}"
+						class="btn btn-circle bg-white border-0">❮</a
+					>
+					<a
+						href="#{idx === slides.length - 1 ? 0 : idx + 1}"
+						class="btn btn-circle bg-white border-0">❯</a
+					>
+				</div>
 			</div>
-		</div>
-	{/each}
-</div>
+		{/each}
+	</div>
+{/if}
