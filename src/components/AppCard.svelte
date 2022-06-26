0bb601625fa6430da0955ddf5630c9038454a1b7
Justin Golden <justingolden21@gmail.com>
2022-06-26 10:58:16 -0700

    AppCard CSS Updates

--- src/components/AppCard.svelte
+++ src/components/AppCard.svelte
@@ -4,14 +4,14 @@
 
 <a href={link} target="_blank" class="card w-96 bg-base-200 shadow-xl cursor-pointer no-underline">
 	<div class="card-body">
-		<h2 class="card-title">
+		<h2 class="card-title text-3xl">
 			{title}
 			{#if isNew}
 				<div class="badge badge-secondary">NEW</div>
 			{/if}
 		</h2>
-		<p>{text}</p>
-		<div class="card-actions justify-end">
+		<p class="my-4">{text}</p>
+		<div class="card-actions">
 			{#each tags as tag}
 				<div class="badge badge-outline">{tag}</div>
 			{/each}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,13 +19,13 @@
 	<article class="prose lg:prose-xl">
 		<h1 class="text-center">RGB Studios</h1>
 		<p>Hello there</p>
-		<AppCard
-			title="Desktop Clock"
-			text="A simple, resizable customizable, installable clock for any device!"
-			img="https://desktopclock.app/img/screenshot.png"
-			link="https://desktopclock.app/"
-			isNew={true}
-			tags={['clock', 'stopwatch', 'worldclock', 'web app', 'installable', 'free', 'offline']}
-		/>
 	</article>
+	<AppCard
+		title="Desktop Clock"
+		text="A simple, resizable customizable, installable clock for any device!"
+		img="https://desktopclock.app/img/screenshot.png"
+		link="https://desktopclock.app/"
+		isNew={true}
+		tags={['clock', 'stopwatch', 'worldclock', 'web app', 'installable', 'free', 'offline']}
+	/>
 </div>
