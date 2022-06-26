cf24bce5528cd1e1ebb2de7a472b310ab37cae78
Justin Golden <justingolden21@gmail.com>
2022-06-26 13:45:58 -0700

    Remove old layout code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,9 @@
 <script>
-	// import { onMount } from 'svelte';
-
 	import '../app.css';
 
 	import Navbar from '../components/Navbar.svelte';
 	import RGBHero from '../components/RGBHero.svelte';
 	import Footer from '../components/Footer.svelte';
-
-	// onMount(() => {
-	// 	// random primary color. r,g, or b
-	// 	const randomColor = ['150 80% 40%', '225 100% 60%', '15 100% 40%'][
-	// 		Math.floor(Math.random() * 3)
-	// 	];
-	// 	document.querySelector(':root').style.setProperty('--p', randomColor);
-	// });
 </script>
 
 <Navbar />
