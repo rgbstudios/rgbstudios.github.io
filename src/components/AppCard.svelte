9fa8f4c264d3a4d387b4cc3eaf1008ef003a9617
Justin Golden <justingolden21@gmail.com>
2022-06-26 11:32:57 -0700

    Cleanup app card display
    Fits inside grid. No set width for cards. Cards have margin. Image scales to size and crops to top while maintaining proportion (100% width, specific height, object fit cover)

--- src/components/AppCard.svelte
+++ src/components/AppCard.svelte
@@ -2,7 +2,7 @@
 	export let title, text, img, link, isNew, tags;
 </script>
 
-<a href={link} target="_blank" class="card w-96 bg-base-200 shadow-xl cursor-pointer no-underline">
+<a href={link} target="_blank" class="card bg-base-200 shadow-xl cursor-pointer no-underline m-4">
 	<div class="card-body">
 		<h2 class="card-title text-3xl">
 			{title}
@@ -17,5 +17,5 @@
 			{/each}
 		</div>
 	</div>
-	<figure><img src={img} alt={title} /></figure>
+	<figure><img src={img} alt={title} class="w-full h-72 object-cover object-top" /></figure>
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,7 +22,9 @@
 		<h1 class="text-center">RGB Studios</h1>
 		<p>Hello there</p>
 	</article>
-	{#each projects as { title, text, img, link, isNew, tags }}
-		<AppCard {title} {text} {img} {link} {isNew} {tags} />
-	{/each}
+	<div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		{#each projects as { title, text, img, link, isNew, tags }}
+			<AppCard {title} {text} {img} {link} {isNew} {tags} />
+		{/each}
+	</div>
 </div>
