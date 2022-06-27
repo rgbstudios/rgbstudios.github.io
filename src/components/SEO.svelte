70b17333515ac3a0518b9ed68371054f2547902b
Justin Golden <justingolden21@gmail.com>
2022-06-26 23:44:53 -0700

    Use `$page.url.host` in SEO
    sitemap and robots are plain js files, will work in prod anyway

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -2,6 +2,7 @@
 	// @see open graph protocol https://ogp.me/
 
 	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 
 	export let title = 'RGB Studios',
 		description = 'RGB Studios makes simple, useful websites, webapps, and mobile apps.',
@@ -29,18 +30,16 @@
 	onMount(() => {
 		if (!url) url = window.location.href;
 	});
-
-	const BASE_URL = 'https://rgbstudios.org/';
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 
-	<link rel="icon" href="{BASE_URL}img/rgb_logo_64.png" />
+	<link rel="icon" href="{$page.url.host}img/rgb_logo_64.png" />
 	<meta name="apple-mobile-web-app-status-bar" content="#111111" />
 	<meta name="theme-color" content="#111111" />
 	<meta name="msapplication-TileColor" content="#111111" />
-	<link rel="mask-icon" href="{BASE_URL}img/rgb_logo_64.png" color="#111111" />
+	<link rel="mask-icon" href="{$page.url.host}img/rgb_logo_64.png" color="#111111" />
 
 	<meta name="title" content={title} />
 	<meta name="description" content={description} />
@@ -53,19 +52,19 @@
 	<meta property="og:description" content={description} />
 	<meta property="og:title" content={title} />
 	<meta property="og:url" content={url} />
-	<meta property="og:image" content={BASE_URL + screenshot} />
+	<meta property="og:image" content={$page.url.host + screenshot} />
 	<meta property="og:site_name" content={title} />
 
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={BASE_URL + screenshot} />
+	<meta name="twitter:image" content={$page.url.host + screenshot} />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content={url} />
 
 	<link rel="canonical" href={url} />
 
 	{#if articleData !== null}
-		<meta property="article:publisher" content={BASE_URL} />
+		<meta property="article:publisher" content={$page.url.host} />
 		<meta property="article:published_time" content={articleData.published_time} />
 		<meta property="article:modified_time" content={articleData.modified_time} />
 		<meta property="article:author" content={articleData.author} />
