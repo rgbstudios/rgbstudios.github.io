8b41b00acc3d2a1103b59bfda7c12ac4503934dc
Justin Golden <justingolden21@gmail.com>
2022-06-28 23:45:59 -0700

    Create ProjectHeader.svelte

---
+++ src/components/ProjectHeader.svelte
@@ -0,0 +1,31 @@
+<script>
+	import SEO from './SEO.svelte';
+	import Breadcrumbs from './Breadcrumbs.svelte';
+
+	export let title, description, keywords, icon, screenshot;
+</script>
+
+<div />
+
+<SEO title="{title} | RGB Studios" {description} {keywords} {icon} {screenshot} />
+
+<Breadcrumbs
+	breadcrumbs={[
+		{
+			text: 'Home',
+			link: '/'
+		},
+		{
+			text: 'Projects',
+			link: '/projects'
+		},
+		{
+			text: title
+		}
+	]}
+/>
+
+<article class="prose lg:prose-xl mx-auto">
+	<img class="mx-auto" src="/{icon}" width="64px" alt="" />
+	<h1 class="text-center">{title}</h1>
+</article>
