a972779ffd02594f228ba6df262d5b4993b99ff8
Justin Golden <justingolden21@gmail.com>
2022-06-28 23:27:37 -0700

    Small footer layout for projects pages

---
+++ src/routes/projects/__layout@reset.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../../app.css';
+
+	import SmallFooter from '../../components/SmallFooter.svelte';
+</script>
+
+<div class="container mx-auto p-8">
+	<slot />
+</div>
+
+<SmallFooter />
