46143aecec5090a0e7d1f064d9fa4e4266b491d1
Justin Golden <justingolden21@gmail.com>
2022-06-30 10:26:23 -0700

    Intersect calc cleanup

--- src/routes/projects/intersect-calc.svelte
+++ src/routes/projects/intersect-calc.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Icon from '../../components/Icon.svelte';
 	import ProjectHeader from '../../components/ProjectHeader.svelte';
-	import Alert from '../../components/Alert.svelte';
 
 	let m1 = 1;
 	let m2 = 1;
@@ -18,25 +16,25 @@
 
 <ProjectHeader
 	title="Intersect Calc"
-	description="Find intersection of two lines"
+	description="Find the intersection point of two lines"
 	keywords={[
 		'intersection calculator',
 		'calculate intersection of two lines',
 		'find out where two lines intersect'
 	]}
 	icon="img/projects/icons/intersect-calc.png"
-	screenshot="img/projects/screenshots/base-convert.png"
+	screenshot="img/projects/screenshots/intersect-calc.png"
 />
 <div class="flex flex-col gap-5">
-	<div class="form-control">
-		<label class="label">
+	<div>
+		<span class="label">
 			<span class="label-text">Line 1 Equation</span>
-		</label>
+		</span>
 		<label class="input-group">
 			<span>y = </span>
 			<input
 				type="number"
-				placeholder="value of slope here (m)"
+				placeholder="slope (m)"
 				bind:value={m1}
 				class="input input-bordered text-center"
 			/>
@@ -44,21 +42,21 @@
 
 			<input
 				type="number"
-				placeholder="value of y - intercept here (b)"
+				placeholder="y-intercept (b)"
 				bind:value={b1}
 				class="input input-bordered text-center"
 			/>
 		</label>
 	</div>
-	<div class="form-control">
-		<label class="label">
+	<div>
+		<span class="label">
 			<span class="label-text">Line 2 Equation</span>
-		</label>
+		</span>
 		<label class="input-group">
 			<span>y = </span>
 			<input
 				type="number"
-				placeholder="value of slope here (m)"
+				placeholder="slope (m)"
 				bind:value={m2}
 				class="input input-bordered text-center"
 			/>
@@ -66,35 +64,33 @@
 
 			<input
 				type="number"
-				placeholder="value of y - intercept here (b)"
+				placeholder="y-intercept (b)"
 				bind:value={b2}
 				class="input input-bordered text-center"
 			/>
 		</label>
 	</div>
-	<div class="alert alert-info shadow-lg">
-		<div class="text-2xl font-bold p-5 text-center">
-			{#if isSameLine}
-				<span
-					>Both the slopes and the y-intercept are same therefore both lines are the same and will
-					intersect at every point on either line</span
-				>
-			{:else if hasSameIntercept}
-				<span
-					>The slopes are different but the y-intercept are same therefore both lines will intersect
-					at the y-intercept (0, {b1})</span
-				>
-			{:else if isParallel}
-				<span
-					>Slopes are same but y-intercept are different so the lines are parallel and therefore
-					never intersect</span
-				>
-			{:else}
-				<span>Intersection at ({x}, {y})</span>
-			{/if}
-		</div>
+	<div class="alert alert-info text-center text-2xl font-bold p-4">
+		{#if isSameLine}
+			<span>
+				Both the slopes and the y-intercept are same therefore both lines are the same and will
+				intersect at every point on either line
+			</span>
+		{:else if hasSameIntercept}
+			<span>
+				The slopes are different but the y-intercepts are same therefore both lines will intersect
+				at the y-intercept (0, {b1})
+			</span>
+		{:else if isParallel}
+			<span>
+				Slopes are same but y-intercepts are different, so the lines are parallel and therefore
+				never intersect
+			</span>
+		{:else}
+			<span>Intersection at ({x}, {y})</span>
+		{/if}
 	</div>
-	<div class="alert shadow-lg">
+	<div class="alert">
 		<div class="grid text-center justify-center w-full">
 			<h3>Formulas</h3>
 			<p class="leading-loose">
