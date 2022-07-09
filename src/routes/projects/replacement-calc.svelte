89df2676a75691751a9e6b7e63a270ffe5241f30
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-09 12:22:22 +0530

    modals, table, validation

--- src/routes/projects/replacement-calc.svelte
+++ src/routes/projects/replacement-calc.svelte
@@ -9,6 +9,7 @@
 	let m = 4;
 	let n = 5;
 	let k = 1;
+	let btnSubmit;
 
 	$: eq = exactKdistinct(N, m, n, k);
 	$: lt = lessKdistinct(N, m, n, k);
@@ -16,6 +17,13 @@
 	$: le = lt + eq;
 	$: ge = gt + eq;
 	$: mu = mean(N, m, n, k);
+
+	function validateForm() {
+		if (btnSubmit) {
+			N, m, n, k;
+			btnSubmit.click();
+		}
+	}
 
 	$: if ((N || m || n || k) && googleChartsLoaded) {
 		drawCharts();
@@ -30,6 +38,7 @@
 	}
 
 	function nCk(n, k) {
+		if (k > n) return 0;
 		return factorial(n) / (factorial(n - k) * factorial(k));
 	}
 
@@ -156,27 +165,118 @@
 	screenshot="img/projects/screenshots/replacement-calc.png"
 />
 
-<div class="mt-5 flex flex-col gap-5">
-	<label class="input-group">
-		<span>Total number of items</span>
-		<span class="bg-info">N</span>
-		<input type="number" bind:value={N} min="1" max="1000" class="input input-bordered grow" />
-	</label>
-	<label class="input-group">
-		<span>Number of distinct items</span>
-		<span class="bg-info">m</span>
-		<input type="number" bind:value={m} min="1" max="1000" class="input input-bordered grow" />
-	</label>
-	<label class="input-group">
-		<span>Number of items picked</span>
-		<span class="bg-info">n</span>
-		<input type="number" bind:value={n} min="1" max="1000" class="input input-bordered grow" />
-	</label>
-	<label class="input-group">
-		<span>Number of distinct items picked</span>
-		<span class="bg-info">k</span>
-		<input type="number" bind:value={k} min="0" max="1000" class="input input-bordered grow" />
-	</label>
+<div class="overflow-x-auto">
+	<form on:submit|preventDefault>
+		<table class="table w-full border border-gray-700">
+			<tbody>
+				<!-- Input -->
+				<tr>
+					<td>Total number of items</td>
+					<td>N</td>
+					<td
+						><input
+							on:blur={validateForm}
+							type="number"
+							bind:value={N}
+							min="1"
+							max="1000"
+							class="input input-bordered w-full"
+							required
+						/></td
+					>
+				</tr>
+				<tr>
+					<td>Number of distinct items</td>
+					<td>m</td>
+					<td>
+						<input
+							on:blur={validateForm}
+							type="number"
+							bind:value={m}
+							min="1"
+							max="1000"
+							class="input input-bordered w-full"
+							required
+						/>
+					</td>
+				</tr>
+				<tr>
+					<td>Number of items picked</td>
+					<td>n</td>
+					<td>
+						<input
+							on:blur={validateForm}
+							type="number"
+							bind:value={n}
+							min="1"
+							max={N}
+							class="input input-bordered w-full"
+							required
+						/>
+					</td>
+				</tr>
+				<tr>
+					<td>Number of distinct items picked</td>
+					<td>k</td>
+					<td>
+						<input
+							on:blur={validateForm}
+							type="number"
+							bind:value={k}
+							min="1"
+							max={n}
+							class="input input-bordered w-full"
+							required
+						/>
+					</td>
+				</tr>
+				<button type="submit" class="hidden" bind:this={btnSubmit}>Submit</button>
+				<!-- Output -->
+				<tr>
+					<td>Probability exactly k distinct items are picked</td>
+					<td>P(X=k)</td>
+					<td>
+						<input disabled value={eq} class="input input-bordered w-full" />
+					</td>
+				</tr>
+				<tr>
+					<td>Probability less than k distinct items are picked</td>
+					<td>P(X&lt;k)</td>
+					<td>
+						<input disabled value={lt} class="input input-bordered w-full" />
+					</td>
+				</tr>
+				<tr>
+					<td>Probability more than k distinct items are picked</td>
+					<td>P(X&gt;k)</td>
+					<td>
+						<input disabled value={gt} class="input input-bordered w-full" />
+					</td>
+				</tr>
+				<tr>
+					<td>Probability less than or equal to k distinct items are picked</td>
+					<td>P(X&le;k)</td>
+					<td>
+						<input disabled value={le} class="input input-bordered w-full" />
+					</td>
+				</tr>
+				<tr>
+					<td>Probability more than or equal to k distinct items are picked</td>
+					<td>P(X&ge;k)</td>
+					<td>
+						<input disabled value={ge} class="input input-bordered w-full" />
+					</td>
+				</tr>
+				<tr>
+					<td>Mean</td>
+					<td>&mu;</td>
+					<td>
+						<input disabled value={mu} class="input input-bordered w-full" />
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
 </div>
 
 <div class="alert shadow-lg mt-5">
@@ -201,45 +301,120 @@
 	</div>
 </div>
 
-<div class="mt-5 grid md:grid-cols-2 gap-1">
-	<div class="stat bg-gray-900">
-		<div class="stat-title">P(X=k)</div>
-		<div class="stat-value">{eq}</div>
-		<div class="stat-description">Probability exactly k distinct items are picked</div>
-	</div>
-	<div class="stat bg-gray-900">
-		<div class="stat-title">P(X&lt;k)</div>
-		<div class="stat-value">{lt}</div>
-		<div class="stat-description">Cumulative probability less than k distinct items are picked</div>
-	</div>
-	<div class="stat bg-gray-900">
-		<div class="stat-title">P(X&gt;k)</div>
-		<div class="stat-value">{gt}</div>
-		<div class="stat-description">Cumulative probability more than k distinct items are picked</div>
-	</div>
-	<div class="stat bg-gray-900">
-		<div class="stat-title">P(X&le;k)</div>
-		<div class="stat-value">
-			{le}
-		</div>
-		<div class="stat-description">
-			Cumulative probability less than or equal to k distinct items are picked
-		</div>
-	</div>
-	<div class="stat bg-gray-900">
-		<div class="stat-title">P(X&ge;k)</div>
-		<div class="stat-value">
-			{ge}
-		</div>
-		<div class="stat-description">
-			Cumulative probability more than or equal to k distinct items are picked
-		</div>
-	</div>
-	<div class="stat bg-gray-900">
-		<div class="stat-title">Mean(&mu;)</div>
-		<div class="stat-value">{mu}</div>
-	</div>
-</div>
-
 <div id="pie-chart" class="mt-5" />
 <div id="bar-chart" class="mt-5" />
+
+<!-- Info modal -->
+<div class="mt-5">
+	<label for="info-modal" class="btn modal-button btn-info">Info</label>
+
+	<input type="checkbox" id="info-modal" class="modal-toggle" />
+	<label for="info-modal" class="modal cursor-pointer">
+		<label class="modal-box relative text-center" for="">
+			<h3 class="font-bold text-lg">Picking without replacements</h3>
+			<div class="divider" />
+			<p>
+				When picking n items out of N total items, where m of them are distinct, the odds of picking <i
+					>exactly</i
+				>
+				k distinct items is defined as:
+				<br />P(X = k) = <sub>m</sub>C<sub>k</sub> * <sub>N-m</sub>C<sub>n-k</sub> /
+				<sub>N</sub>C<sub>n</sub>
+				<br />
+				<br />Where <sub>n</sub>C<sub>x</sub> ("n choose x") is defined as
+				<br /><sub>n</sub>C<sub>x</sub> = n! / [ x! (n - x)! ]
+				<br />
+				<br />Mean: n * m / N
+			</p>
+
+			<div class="divider" />
+
+			<p>Inputs should be positive integers. Non-integer inputs will be rounded down.</p>
+
+			<p>
+				Picking wihtout replacement means that once you've picked your item, you do not put it back
+				into the pool of items to pick before picking the next item
+			</p>
+
+			<div class="divider" />
+
+			<p>
+				<a
+					class="link"
+					href="https://en.wikipedia.org/wiki/Hypergeometric_distribution"
+					target="_blank">Wikipedia on Hypergeometric Distributions</a
+				>
+			</p>
+			<p>
+				<a class="link" href="https://en.wikipedia.org/wiki/Simple_random_sample" target="_blank"
+					>Wikipedia on Simple Random Samples</a
+				>
+			</p>
+			<p>
+				<a class="link" href="https://en.wikipedia.org/wiki/Urn_problem" target="_blank"
+					>Wikipedia on the Urn Problem</a
+				>
+			</p>
+
+			<br />
+			<p>
+				<a
+					class="link"
+					href="https://web.ma.utexas.edu/users/parker/sampling/woreplshort.htm"
+					target="_blank">UTexas provides formulas for sampling with and without replacement</a
+				>
+			</p>
+		</label>
+	</label>
+
+	<!-- About modal -->
+	<label for="about-modal" class="btn modal-button btn-info">About</label>
+
+	<input type="checkbox" id="about-modal" class="modal-toggle" />
+	<label for="about-modal" class="modal cursor-pointer">
+		<label class="modal-box relative text-center" for="">
+			<h3 class="font-bold text-lg">Picking without replacements</h3>
+			<div class="divider" />
+			<ul>
+				View the data as pie and bar charts, where the odds of
+				<li><span style="color:#339;">exactly k items is represented in <b>blue</b></span>,</li>
+				<li>
+					<span style="color:#933;">less than k items is represented in <b>red</b></span>, and
+				</li>
+				<li>
+					<span style="color:#393;">greater than k items is represented in <n>green</n></span>
+				</li>
+			</ul>
+
+			<br />
+
+			<p>
+				Note that we limit input to 1000. This is because when calculating n choose k for inputs of
+				1000 and 500, we get about 1e300, which is a number larger than should ever been needed, and
+				is just approaching the largest number Javascript will store.
+			</p>
+			<p>
+				To put this into perspective: Say we take every particle in the known universe and multiply
+				that number by the number of nanoseconds the universe has been alive. This number squared is
+				significantly less than 1e300.
+			</p>
+
+			<div class="divider" />
+
+			<p>
+				Made with <a target="_blank" href="https://developers.google.com/chart">Google Charts</a>,
+				<a target="_blank" href="https://material.io/icons/">Material Icons</a>,
+				<a target="_blank" href="https://getbootstrap.com">Bootstrap</a>, and
+				<a target="_blank" href="http://imgbot.net">ImgBot</a>
+			</p>
+
+			<p>
+				View on Github <a href="https://github.com/rgbstudios/replacement-calc" target="_blank"
+					>here</a
+				>
+			</p>
+
+			<p>V.1.0.1</p>
+		</label>
+	</label>
+</div>
