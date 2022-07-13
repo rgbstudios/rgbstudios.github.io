f190db6c045d135d3b411ca1bd63f502cdb7e0cb
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-13 23:19:43 +0530

    close modal on esc and outside click

--- src/lib/components/base/Modal.svelte
+++ src/lib/components/base/Modal.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
 	import Icon from '$lib/components/Icon.svelte';
+	import { onMount } from 'svelte';
 
 	export let id: string;
 	export let hidden: boolean = true;
 	export let showCloseBtn: boolean = false;
+	let inp: HTMLInputElement;
+
+	onMount(() => {
+		document.addEventListener('keyup', function (e: KeyboardEvent) {
+			if (e.key === 'Escape') {
+				inp.checked = false;
+			}
+		});
+	});
 </script>
 
 <input
+	bind:this={inp}
 	type="checkbox"
 	{id}
 	class="modal-toggle"
 	checked={!hidden}
 	on:change={(e) => (hidden = !e.target.checked)}
 />
-<div class="modal">
+<label for={id} class="modal">
 	<div class="modal-box relative rounded border-2">
 		{#if $$slots.title || showCloseBtn}
 			<div class="header flex justify-between items-center border-b-2 p-2 mb-5">
@@ -27,4 +38,4 @@
 		{/if}
 		<slot />
 	</div>
-</div>
+</label>
