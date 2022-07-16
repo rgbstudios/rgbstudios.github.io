d438b02aba0203a541b5f043bb5b40e24500cfc3
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-16 15:25:17 +0530

    move consts to route

--- src/routes/projects/binomial-calc.svelte
+++ src/routes/projects/binomial-calc.svelte
@@ -26,10 +26,38 @@
 	import roundNumber from '$lib/util/roundNumber';
 	import { nCk } from '$lib/util/math';
 	import Icon from '$lib/components/Icon.svelte';
-	import { binomial_calc_info_html } from '$lib/data/consts';
 	import copyText from '$lib/util/copyText';
 
 	/// STATE ///
+
+	const info_html = `
+  <h4>Formulas</h4><br>
+  <p>
+    A binomial experiment with <b>n</b> trials, probability of success <b>p</b>, and <b>x</b> successes, has
+    <br>Binomial Probability: $$P(X = x) = _nC_x * p^x * (1 - p)^{n - x}$$
+    <br>Where "n choose x" is: $$(_nC_x) = \\frac{n!}{x! (n - x)!}$$
+    <br>
+    <br>Mean: $$ \\mu = n * p$$
+    <br>Variance: $$ \\sigma = n * p * (1-p)$$
+    <br>Standard Deviation: $$ \\sigma^2 = \\sqrt{n * p * (1-p)}$$
+  </p>
+
+  <div class="divider"></div>
+  <h4>Requirements</h4><br>
+
+  <p>Binomial Experiments must have:</p>
+  <ul>
+    <li><b>Trials with 2 outcomes</b> (success or failure)</li>
+    <li><b>Independent trials</b> (the probability of a particular outcome is constant)</li>
+  </ul>
+
+  <br><p><b>p</b> must be between 0 and 1, <b>n</b> and <b>x</b> must be positive integers where <b>n</b> &ge; <b>x</b></p>
+
+  <div class="divider"></div>
+
+  <p>Learn more on <a class="link" href="https://en.wikipedia.org/wiki/Binomial_distribution" target="_blank">Wikipedia</a>
+  and <a class="link" href="https://www.mathsisfun.com/data/binomial-distribution.html" target="_blank">Math is Fun</a>, or watch a <a class="link" href="https://youtu.be/8idr1WZ1A7Q" target="_blank">3Blue1Brown video</a>.</p>
+`;
 	let changed = false;
 	$: {
 		p, n, x;
@@ -461,7 +489,7 @@
 		<label class="modal-box relative" for="">
 			<h3 class="font-bold text-lg">Binomial Distributions</h3>
 			<div class="divider" />
-			{@html binomial_calc_info_html}
+			{@html info_html}
 			<!-- Link to this modal: -->
 			<div class="mt-5 grid gap-1">
 				<span>Share the link to these formulas:</span>
