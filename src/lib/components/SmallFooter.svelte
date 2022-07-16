f274c0a619faa873e745cd68030684d6054e020b
Justin Golden <justingolden21@gmail.com>
2022-07-16 16:46:01 -0700

    Wrap footer in container

--- src/lib/components/SmallFooter.svelte
+++ src/lib/components/SmallFooter.svelte
@@ -3,23 +3,25 @@
 	const year = new Date().getFullYear();
 </script>
 
-<footer class="footer items-center p-4 bg-base-200">
-	<div class="items-center grid-flow-col">
-		<img class="w-12 h-12" src="/img/rgb_logo_64.png" alt="RGB logo" />
-		<p>
-			<a class="link link-hover font-bold" href="/">RGB Studios</a>
-			<br />
-			Simple webapps and more
-			<br />
-			&copy; Copyright {year}
-		</p>
-	</div>
-	<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-		<a href="https://twitter.com/rgbstudiosorg" target="_blank">
-			<Icon name="twitter" />
-		</a>
-		<a href="https://www.facebook.com/rgbstudios.org" target="_blank">
-			<Icon name="facebook" />
-		</a>
-	</div>
-</footer>
+<div class="bg-base-200">
+	<footer class="container mx-auto footer items-center p-4">
+		<div class="items-center grid-flow-col">
+			<img class="w-12 h-12" src="/img/rgb_logo_64.png" alt="RGB logo" />
+			<p>
+				<a class="link link-hover font-bold" href="/">RGB Studios</a>
+				<br />
+				Simple webapps and more
+				<br />
+				&copy; Copyright {year}
+			</p>
+		</div>
+		<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+			<a href="https://twitter.com/rgbstudiosorg" target="_blank">
+				<Icon name="twitter" />
+			</a>
+			<a href="https://www.facebook.com/rgbstudios.org" target="_blank">
+				<Icon name="facebook" />
+			</a>
+		</div>
+	</footer>
+</div>
