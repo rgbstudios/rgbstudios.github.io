b23f99f018942a95034edfcfbb52c76f0c64dcf9
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-21 13:18:23 +0530

    fix print

--- src/lib/components/modals/barcode/WifiQRModal.svelte
+++ src/lib/components/modals/barcode/WifiQRModal.svelte
@@ -1,23 +1,62 @@
 <script lang="ts">
+	/// STATE ///
 	import QRCode from 'qrcode';
 	import Modal from '$lib/components/base/Modal.svelte';
 	import Icon from '$lib/components/Icon.svelte';
-
-	let canvas: HTMLCanvasElement;
-	import { bgColor, linesColor, margin } from '$lib/stores/barcode';
 	import ModalButton from '$lib/components/base/ModalButton.svelte';
 	import WifiScanHelpModal from './WifiScanHelpModal.svelte';
-	let qrDataURL: string;
+
+	/// STATE ///
+	import { bgColor, linesColor, margin } from '$lib/stores/barcode';
 	export let hidden: boolean = true;
+	let canvas: HTMLCanvasElement;
+	let qrDataURL: string;
 	let auth: 'WPA' | 'WEP' = 'WPA';
 	let name: string = '';
 	let pass: string = '';
+	let showName: boolean = false;
+	let showPass: boolean = false;
 	$: wifiStr = `WIFI:T:${auth};S:${name};P:${pass};;`;
 	$: errorMsg = name && pass ? '' : 'Please enter a name and a password';
 
 	/// METHODS ///
 	function download() {
 		qrDataURL = canvas.toDataURL('image/png');
+	}
+
+	function print() {
+		qrDataURL = canvas.toDataURL('image/png');
+		const win = window.open('', '', '');
+		win.document.write(`<style type="text/css">
+			* {
+				text-align: center;
+				font: 32px Arial;
+				line-height: 2.5rem;
+			}
+			b {
+				font-weight: bold; /*idk...*/
+			}
+			img:not(.icon) {
+				padding: 0.5rem;
+				margin: 1rem;
+				border: 4px solid black;
+				box-shadow: 0.5rem 0.5rem #999;
+			}
+		</style>`);
+		win.document.write(`<b>Scan to automatically connect to our Wifi</b> <br>`);
+		win.document.write(`<img src='${qrDataURL}'></img>`);
+		if (showName) win.document.write(`<br><b>Network:</b> ${name}`);
+		if (showPass) win.document.write(`<br><b>Password:</b> ${pass}`);
+		win.document.close();
+		win.addEventListener(
+			'load',
+			() => {
+				win.focus();
+				win.print();
+				win.close();
+			},
+			false
+		);
 	}
 
 	$: canvas &&
@@ -28,15 +67,6 @@
 				dark: $linesColor
 			}
 		});
-
-	function print() {
-		qrDataURL = canvas.toDataURL('image/png');
-		const win = window.open(qrDataURL, '_blank');
-		const iframe = `<iframe style='border: none; outline: none' width='100%' height='100%' src='${qrDataURL}'></iframe>`;
-		win.document.write(iframe);
-		win.print();
-		win.close();
-	}
 </script>
 
 <Modal id="barcode-wifi-qr-modal" bind:hidden fluid>
@@ -57,6 +87,21 @@
 			<span>Password</span>
 			<input type="password" class="grow input input-bordered" bind:value={pass} />
 		</label>
+		<div class="col-span-full grid gap-5 mx-auto">
+			<h3 class="flex gap-3">
+				<Icon name="settings" />Print Settings
+			</h3>
+			<div class="gap-5 grid">
+				<label class="flex items-center gap-3 cursor-pointer">
+					<input type="checkbox" bind:checked={showName} class="checkbox" />
+					<span class="label-text">Show Name</span>
+				</label>
+				<label class="flex items-center gap-3 cursor-pointer">
+					<input type="checkbox" bind:checked={showPass} class="checkbox" />
+					<span class="label-text">Show Password</span>
+				</label>
+			</div>
+		</div>
 	</div>
 	{#if errorMsg === ''}
 		<div class="grid p-5 justify-center">
