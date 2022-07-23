596b0d39728c67778a886e6731214b9166881fbd
Justin Golden <justingolden21@gmail.com>
2022-07-22 21:59:14 -0700

    Local store bug fix

--- src/lib/stores/barcode.ts
+++ src/lib/stores/barcode.ts
@@ -2,7 +2,7 @@ import { barcodeFormats } from '$lib/data/consts';
 import localStore from '$lib/util/localStore';
 
 export const text = localStore<string>('barcode-text', 'Hello World!');
-export const format = localStore<string>('barcode-format', barcodeFormats[0]);
+export const format = localStore<string>('barcode-format', barcodeFormats[0][0]);
 export const showLabel = localStore<boolean>('barcode-show-label', true);
 export const linesColor = localStore<string>('barcode-lines-color', '#000000');
 export const bgColor = localStore<string>('barcode-bg-color', '#FFFFFF');
