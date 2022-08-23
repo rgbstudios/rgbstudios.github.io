b852a8c805f52b38071245cee2e5a22660f28855
Justin Golden <justingolden21@gmail.com>
2022-08-22 23:42:20 -0700

    Add project header to dnd pages

--- src/routes/projects/dnd-dice/character-roller.svelte
+++ src/routes/projects/dnd-dice/character-roller.svelte
@@ -1,5 +1,38 @@
 <script>
+	import ProjectHeader from '$lib/components/ProjectHeader.svelte';
+
 	import DndSideNav from '$lib/components/dnd-dice/DndSideNav.svelte';
 </script>
 
 <DndSideNav />
+
+<!-- todo d6 icon
+topdo update screenshot -->
+<ProjectHeader
+	title="D&D Character Roller"
+	description="Use D&D Character Roller to easily roll stats for a new D&D character, complete with graphs, download, and more"
+	keywords={[
+		'dnd',
+		'd&d',
+		'character',
+		'd&d character',
+		'dnd character',
+		'character roller',
+		'character stat',
+		'stat roller',
+		'character generator',
+		'dungeons and dragons',
+		'dice',
+		'die',
+		'roller',
+		'roll',
+		'dice roller',
+		'die roller'
+	]}
+	icon="img/projects/icons/dnd-dice.png"
+	screenshot="img/projects/screenshots/dnd-dice.png"
+	isDark={true}
+	isCondensed={true}
+/>
+
+Coming soon...
--- src/routes/projects/dnd-dice/spellbook.svelte
+++ src/routes/projects/dnd-dice/spellbook.svelte
@@ -1,5 +1,31 @@
 <script>
+	import ProjectHeader from '$lib/components/ProjectHeader.svelte';
+
 	import DndSideNav from '$lib/components/dnd-dice/DndSideNav.svelte';
 </script>
 
 <DndSideNav />
+
+<!-- todo d8 icon
+topdo update screenshot -->
+<ProjectHeader
+	title="D&D Spellbook"
+	description="Search for D&D spells and find description, range, class, level, and more"
+	keywords={[
+		'dnd',
+		'd&d',
+		'dnd spell',
+		'd&d spell',
+		'dnd5e',
+		'spell',
+		'spellbook',
+		'spell',
+		'searcher'
+	]}
+	icon="img/projects/icons/dnd-dice.png"
+	screenshot="img/projects/screenshots/dnd-dice.png"
+	isDark={true}
+	isCondensed={true}
+/>
+
+Coming soon...
