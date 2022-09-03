d7f800dcc75e1be1c720a92ed7a632f63b9f9926
Jack van der Bilt <jack@managing.software>
2022-09-03 08:57:18 +0200

    docs: explain `substring(3,length-9)`

--- src/routes/(rgb)/blog/posts.json/+server.js
+++ src/routes/(rgb)/blog/posts.json/+server.js
@@ -2,7 +2,7 @@ export async function GET() {
 	const posts = await Promise.all(
 		Object.entries(import.meta.glob('../**/*.md')).map(async ([path, page]) => {
 			const { metadata } = await page();
-			const slug = path.substring(3, path.length - 9);
+			const slug = path.substring(3, path.length - 9); // `3` removes `../` prefix; `9` removes `+page.svelte` suffix.
 			return { ...metadata, slug };
 		})
 	);
