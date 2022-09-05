263d353cd8d69c35d939d4190d8270ea846601cf
Jack van der Bilt <jack@managing.software>
2022-09-05 21:47:54 +0200

    refactor: remove deconstruction of response object

--- src/routes/(rgb)/+page.js
+++ src/routes/(rgb)/+page.js
@@ -1,6 +1,5 @@
 export async function load({ fetch }) {
 	const res = await fetch('/blog/posts.json');
-	const { posts } = await res.json();
-
+	const posts = await res.json();
 	return { posts };
 }
