5204f027c110f18fc844f7401c64d49b55bdd948
Justin Golden <justingolden21@gmail.com>
2023-01-16 02:06:06 -0800

    Fix generic error (non 404) page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script>
+	/**
+	 * Used for non-404 errors
+	 *
+	 * Test this page by creating an invalid import in homepage
+	 * For example:
+	 * `import SEO from '$lib/components/SEO.svelteee';`
+	 */
+
+	import '$lib/app.css';
+
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Error | RGB Studios</title>
+</svelte:head>
+
+<article class="prose-custom mx-auto my-8">
+	<h1>Error {$page.status} &mdash; {$page.error.message}</h1>
+
+	<span class="not-prose"><a href="/" class="btn btn-primary no-underline">Go back home</a></span>
+
+	<p>
+		If this problem persists, please contact us at <a href="mailto:contact@rgbstudios.org">
+			contact@rgbstudios.org
+		</a>.
+	</p>
+</article>
