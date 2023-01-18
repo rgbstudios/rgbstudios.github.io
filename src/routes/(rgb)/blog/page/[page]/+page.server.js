763c86d5fb338299f0730d4654e9335153c4ba75
Justin Golden <justingolden21@gmail.com>
2023-01-17 23:41:41 -0800

    Basic /blog/page/[number] logic

---
+++ src/routes/(rgb)/blog/page/[page]/+page.server.js
@@ -0,0 +1,30 @@
+import { error } from '@sveltejs/kit';
+
+const postsPerPage = 12;
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+	// If non-numeric values, 404
+	if (!new RegExp(/^[0-9]+$/).test(params.page)) {
+		throw error(404, 'Not found');
+	}
+	// If page number is 0 or contains a leading 0, 404
+	if (params.page[0] === '0') {
+		throw error(404, 'Not found');
+	}
+
+	const pageNum = parseInt(params.page);
+
+	// TODO: fixme
+	const res = await fetch('http://localhost:5173/blog/posts.json');
+	const posts = await res.json();
+
+	const pagePosts = posts.splice(postsPerPage * (pageNum - 1), postsPerPage);
+
+	// Invalid page number, 404
+	if (pagePosts.length === 0) {
+		throw error(404, 'Not found');
+	}
+
+	return { posts: pagePosts, pageNum };
+}
---
+++ src/routes/(rgb)/blog/page/[page]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import BlogGrid from '$lib/components/BlogGrid.svelte';
+
+	export let data;
+
+	$: posts = data.posts;
+	$: pageNum = data.pageNum;
+</script>
+
+<svetle:head>
+	<title>Blog Page {pageNum} | RGB Studios</title>
+</svetle:head>
+
+<article class="prose-custom mx-auto my-8">
+	<h1 class="text-center">Blog Page {pageNum}</h1>
+
+	<BlogGrid {posts} />
+</article>
