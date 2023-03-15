329c8adccb8887abb5392948e7e76c346a6b61e2
Justin Golden <justingolden21@gmail.com>
2023-03-15 14:36:13 -0700

    Update blog grid breakpoint

--- src/lib/components/BlogGrid.svelte
+++ src/lib/components/BlogGrid.svelte
@@ -3,7 +3,7 @@
 	export let posts;
 </script>
 
-<div class="grid gap-8 my-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+<div class="grid gap-8 my-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 	{#each posts as { title, preview_text, slug, categories, img } (slug)}
 		<BlogCard {title} text={preview_text} link="/blog/{slug}" {img} tags={categories} />
 	{/each}
