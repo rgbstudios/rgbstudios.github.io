63d8409abdef15957322a356035e5ca5244db4ed
Justin Golden <justingolden21@gmail.com>
2024-05-18 15:36:46 -0700

    Remove unused function factorial
    Not sure why this wasn't detected by lint

--- src/lib/util/math.ts
+++ src/lib/util/math.ts
@@ -4,14 +4,6 @@
  * Currently, only used by binomial calc
  */
 
-export function factorial(n: number): number {
-	let retval = 1;
-	for (let i = n; i > 1; i--) {
-		retval *= i;
-	}
-	return retval;
-}
-
 export function nCk(n: number, k: number): number {
 	let result = 1;
 	for (let i = 1; i <= k; i++) {
