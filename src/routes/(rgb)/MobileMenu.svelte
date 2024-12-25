cd19e7c93bd9901f1fd3e558d743408ad7235ae4
Jack van der Bilt <jack@managing.software>
2024-12-26 00:24:00 +0100

    refactor: improve dropdown UX and code

---
+++ src/routes/(rgb)/MobileMenu.svelte
@@ -0,0 +1,28 @@
+<script>
+	import MobileMenuDropdown from './MobileMenuDropdown.svelte';
+	import Icon from '$lib/components/Icon.svelte';
+
+	let mobileMenuDropdownParent;
+	let isMobileMenuDropdownOpen = false;
+</script>
+
+<svelte:window
+	on:resize={() => (isMobileMenuDropdownOpen = false)}
+	on:click={({ target }) => {
+		if (!mobileMenuDropdownParent.contains(target)) {
+			isMobileMenuDropdownOpen = false;
+		}
+	}}
+/>
+<div class="relative lg:hidden" bind:this={mobileMenuDropdownParent}>
+	<button
+		class="btn btn-ghost"
+		title="Menu"
+		on:click={() => (isMobileMenuDropdownOpen = !isMobileMenuDropdownOpen)}
+	>
+		<Icon name="menu" />
+	</button>
+	<MobileMenuDropdown {isMobileMenuDropdownOpen}>
+		<slot />
+	</MobileMenuDropdown>
+</div>
---
+++ src/routes/(rgb)/MobileMenuDropdown.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let isMobileMenuDropdownOpen = false;
+</script>
+
+<ul
+  class="absolute menu menu-compact dropdown-content mt-2 p-2 shadow bg-base-900 rounded-box w-52 origin-top-left transition-all ease-in-out duration-200 {isMobileMenuDropdownOpen ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}"
+>
+  <slot />
+</ul>
